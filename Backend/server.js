b795c0f2e2cd5e454b1126ebc7392a2413919568
Dileep <dileepkumawat525@gmail.com>
2026-03-15 22:15:22 +0530

    add email verification during registration and implement login functionality

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 import 'dotenv/config';
 import app from './src/app.js';
 import { connectToDb } from './src/config/database.js';
+import { testAi } from './src/services/ai.service.js';
 
 connectToDb();
+
+testAi();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
