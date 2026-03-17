ccc1b389745cca51bb06b4c2f5d15a1f04391224
Dileep <dileepkumawat525@gmail.com>
2026-03-17 22:12:16 +0530

    socket io initialized.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js';
 import cors from 'cors';
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan("dev"));
 app.use(cors({
     origin: process.env.FRONTEND_ENDPOINT,
     credentials: true
