ebd53eb9f1f40a37c85238bf6e967b15f0de38ff
Dileep <dileepkumawat525@gmail.com>
2026-03-20 18:37:29 +0530

    changed the typo at database configurations.

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export async function connectToDb() {
-    await mongoose.connect(process.env.MONGOdB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("The Database is connected");
 }
